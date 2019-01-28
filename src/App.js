793a3ca4c5f291387eb2ea145a034753f9446ebd
Anders Lundback <anders.lundback@postoffice.co.uk>
2019-01-28 17:24:28 +0000

    mark up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+var _ = require('lodash');
+
+const Stars = (props) => {
+  return (
+    <div className="col-5">
+      {_.range(props.numberOfStars).map(i =>
+        <i key={i} className="fa fa-star"></i>
+      )}
+    </div>
+  );
+}
+
+const Button = (props) => {
+  return (
+    <div>
+      <button>=</button>
+    </div>
+  );
+}
+
+const Answer = (props) => {
+  return (
+    <div className="col-5">
+      {props.selectedNumbers.map((number, i) =>
+        <span key={i}>{number}</span>  
+      )}
+    </div>
+  );
+}
+
+const Numbers = (props) => {
+  const numberClassName = (number) => {
+    if (props.selectedNumbers.indexOf(number) >= 0) {
+      return 'selected';
+    }
+  }
+
+  return (
+    <div className="card text-center">
+      <div>
+        {Numbers.list.map((number, i) =>
+          <span key={i} className={numberClassName(number)}
+            onClick={() => props.selectedNumber(number)}>
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+Numbers.list = _.range(1, 10);
+
+class Game extends React.Component {
+  state = {
+    selectedNumbers: [],
+    randomNumberOfStars: 1 + Math.floor(Math.random()*9),
+  };
+  selectedNumber = (clickedNumber => {
+    if (this.state.selectedNumbers.indexOf(clickedNumber) >= 0) { return; }
+    this.setState(prevState => ({
+      selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
+    }));
+  });
+
+  render() {
+    return (
+      <div className="container">
+          <h3>Play Nine</h3>
+          <hr />
+          <div className="row">
+            <Stars numberOfStars={this.state.randomNumberOfStars} />
+            <Button />
+            <Answer selectedNumbers={this.state.selectedNumbers} />
+            </div>
+            <br />
+            <Numbers selectedNumbers={this.state.selectedNumbers}
+              selectedNumber={this.selectedNumber} />
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Game />
       </div>
     );
   }
